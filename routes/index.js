36322375ef1687fb3c7e963afaa4ca4513052aff
FlorianWinkler <florian.winkler@edu.campus02.at>
2019-05-25 15:30:51 +0200

    added endpoint /preparedb in index.js

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,9 @@ router.get('/', function(req, res, next) {
   res.send('NodeJS + Express läuft!');
 });
 
+router.get('/preparedb', function(req, res, next) {
+  util.prepareDatabase();
+  res.send('Populating monolithic DB...');
+});
+
 module.exports = router;
